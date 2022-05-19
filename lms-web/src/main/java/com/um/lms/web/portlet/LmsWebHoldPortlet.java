9c712be2acbe941455aaffffb4f1e00d83b08a6c
vadivel <avkannanjuly@gmail.com>
2022-05-19 13:13:06 +0800

    [#LMS_US_08] search

---
+++ lms-web/src/main/java/com/um/lms/web/portlet/LmsWebHoldPortlet.java
@@ -0,0 +1,62 @@
+package com.um.lms.web.portlet;
+
+import com.um.lms.sb.model.student;
+import com.um.lms.sb.service.studentLocalService;
+import com.um.lms.sb.service.studentLocalServiceUtil;
+import com.um.lms.web.constants.LmsWebPortletKeys;
+import com.liferay.counter.kernel.service.CounterLocalServiceUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.util.ParamUtil;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.Portlet;
+import javax.portlet.ProcessAction;
+
+import org.osgi.service.component.annotations.Component;
+
+import aQute.bnd.annotation.component.Reference;
+
+/**
+ * @author vadiv
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + LmsWebPortletKeys.LmsWeb,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class LmsWebHoldPortlet extends MVCPortlet {
+	
+	
+private Log log = LogFactoryUtil.getLog(this.getClass().getName());
+    
+ 
+    @ProcessAction(name = "holdbook")
+    public void holdbook(ActionRequest actionRequest,ActionResponse actionResponse) {
+        long studentId = CounterLocalServiceUtil.increment(student.class.getName());
+        String enrollmentNo = ParamUtil.getString(actionRequest, "enrollmentNo");
+        String firstName = ParamUtil.getString(actionRequest, "firstName");
+        String lastName = ParamUtil.getString(actionRequest, "lastName");
+        String contactNo = ParamUtil.getString(actionRequest, "contactNo");
+        String city = ParamUtil.getString(actionRequest, "city");
+    
+        student student = studentLocalServiceUtil.createstudent(CounterLocalServiceUtil.increment());
+        student.setFirstName(firstName);
+        student.setLastName(lastName);
+        log.info("created");
+        studentLocalServiceUtil.addstudent(student);
+    }
+    
+
+}
